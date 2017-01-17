9f22eab7eb590ae1b583d6f46a5dd4bdc12f6ede
Will Storey <will@summercat.com>
2017-01-17 11:29:11 -0800

    gorselib: Clean up comments
    Generally just line lengths and capitalizing updates.

--- gorselib/gorselib.go
+++ gorselib/gorselib.go
@@ -1,7 +1,5 @@
-//
-// Package gorselib provides helper function for interacting with RSS feeds.
-// primarily this surrounds building and reading/parsing.
-//
+// Package gorselib provides helper function for interacting with RSS, RDF,
+// and Atom feeds. Primarily this surrounds building and reading/parsing.
 package gorselib
 
 import (
@@ -23,34 +21,35 @@ import (
 
 // Config controls package wide settings.
 type Config struct {
-	// control whether we have verbose output (or not)
+	// Control whether we have verbose output (or not).
 	Quiet bool
 }
 
-// use a global default set of settings.
-// see package log for a similar approach (global default settings)
+// Use a global default set of settings.
+//
+// See package log for a similar approach (global default settings).
 var config = Config{
 	Quiet: false,
 }
 
 // RSSFeed describes a feed.
 type RSSFeed struct {
-	// database id of the feed.
+	// Database id of the feed.
 	ID int64
 
-	// name of the feed.
+	// Name of the feed.
 	Name string
 
-	// description of the fed.
+	// Description of the fed.
 	Description string
 
-	// uri to the feed.
+	// URI to the feed.
 	URI string
 
-	// update frequency in seconds.
+	// Update frequency in seconds.
 	UpdateFrequencySeconds int64
 
-	// last time we updated.
+	// Last time we updated.
 	LastUpdateTime time.Time
 
 	Items []RSSItem
@@ -72,8 +71,8 @@ type RSSItem struct {
 	ReadState             string
 }
 
-// Item is abstracted from ItemXML and RDFItemXML.
-// Why? Because I will convert ItemXML and RDFItemXML both into this form.
+// Item is abstracted from ItemXML and RDFItemXML. Why? Because I will convert
+// ItemXML and RDFItemXML both into this form.
 type Item struct {
 	Title       string
 	Link        string
@@ -82,9 +81,8 @@ type Item struct {
 	GUID        string
 }
 
-// Channel is abstracted from ChannelXML.
-// Why? Because I will convert ItemXML and RDFItemXML both into Item and need
-// to then store it here.
+// Channel is abstracted from ChannelXML. Why? Because I will convert ItemXML
+// and RDFItemXML both into Item and need to then store it here.
 type Channel struct {
 	Title         string
 	Link          string
@@ -94,7 +92,7 @@ type Channel struct {
 	Items         []Item
 }
 
-// ItemXML and the other *XML structs are for parsing the XML. We fill them up
+// ItemXML and the other *XML structs are for parsing the XML. We fill them
 // with data we parse out. However, they are also specially set up for the
 // xml.Unmarshal() function (i.e., the tags on each field)
 type ItemXML struct {
@@ -124,7 +122,7 @@ type RDFItemXML struct {
 	Link        string   `xml:"link"`
 	Description string   `xml:"description"`
 	PubDate     string   `xml:"date"`
-	// TODO: We don't need GUID? rdf does not have it
+	// TODO: We don't need GUID? RDF does not have it
 	GUID string `xml:"guid"`
 }
 
@@ -153,30 +151,36 @@ func SetQuiet(quiet bool) {
 	config.Quiet = quiet
 }
 
-// SanitiseItemText takes text (e.g., title or description) and removes
-// any html markup. this is because some feeds (e.g., slashdot) include
-// a lot of markup I don't want to actually show.
+// SanitiseItemText takes text (e.g., title or description) and removes any HTML
+// markup. This is because some feeds (e.g., Slashdot) include a lot of markup
+// I don't want to actually show.
 //
-// we also un-decode html entities since apparently we can get these through
-// to this point (they will be encoded again as necessary when we render the
-// page). for example in a raw xml from slashdot we have this:
+// We also decode HTML entities since apparently we can get these through to
+// this point (they will be encoded again as necessary when we render the
+// page).
+//
+// For example in a raw XML from Slashdot we have this:
+//
 // <item><title>AT&amp;amp;T Gets Patent To Monitor and Track File-Sharing Traffic</title>
-// which gets placed into the database as:
+//
+// Which gets placed into the database as:
 // AT&amp;T Gets Patent To Monitor and Track File-Sharing Traffic
 //
-// this can be used to take any string which has html in it to clean
-// up that string and make it non-html.
+// This can be used to take any string which has HTML in it to clean up that
+// string and make it non-HTML.
 //
-// NOTE: while elements such as 'title' can have html in them, this seems
-//   applied inconsistently. for instance, consider this title from
-//   a slashdot feed:
-//   <title>Google Maps Updated With Skyfall&lt;/em&gt; Island Japan Terrain</title>
-//   that is: </em> in there but no <em>.
-//   in the database this is present as </em>.
+// While elements such as 'title' can have HTMLin them, this seems applied
+// inconsistently. For instance, consider this title from a Slashdot feed:
 //
-//   thus we do not place the html into the page raw.
+// <title>Google Maps Updated With Skyfall&lt;/em&gt; Island Japan Terrain</title>
+//
+// That is: </em> in there but no <em>.
+//
+// In the database this is present as </em>.
+//
+// Thus we do not place the HTML into the page raw.
 func SanitiseItemText(text string) (string, error) {
-	// first remove raw html.
+	// First remove raw HTML.
 	re, err := regexp.Compile("(?s)<.*?>")
 	if err != nil {
 		log.Printf("Failed to compile html regexp: %s", err)
@@ -184,10 +188,10 @@ func SanitiseItemText(text string) (string, error) {
 	}
 	text = re.ReplaceAllString(text, "")
 
-	// then undecode html entities.
+	// Decode HTML entities.
 	text = html.UnescapeString(text)
 
-	// turn any multiple spaces into a single space.
+	// Turn any multiple spaces into a single space.
 	re, err = regexp.Compile("(?s)\\s+")
 	if err != nil {
 		log.Printf("Failed to compile whitespace regexp: %s", err)
@@ -199,8 +203,9 @@ func SanitiseItemText(text string) (string, error) {
 }
 
 // GetItemPubDate tries to retrieve a publication date for the item.
-// we try parsing using multiple formats, and fall back to a default
-// of the current time if none succeed.
+//
+// We try parsing using multiple formats, and fall back to a default of the
+// current time if none succeed.
 func GetItemPubDate(pubDate string) time.Time {
 	if len(pubDate) == 0 {
 		if !config.Quiet {
@@ -209,29 +214,32 @@ func GetItemPubDate(pubDate string) time.Time {
 		return time.Now()
 	}
 
-	// use RFC1123 time format for parsing. this appears to be what
-	// is present in the slashdot feed, though I expect this could
-	// vary in other feed sources...
-	// slashdot's feed:
-	// Sat, 29 Jun 2013 18:20:00 GMT
+	// Use RFC1123 time format for parsing. This appears to be what is present in
+	// the Slashdot feed, though I expect this could vary in other feed
+	// sources...
+	//
+	// Slashdot's feed: Sat, 29 Jun 2013 18:20:00 GMT
 	pubDateTimeParsed, err := time.Parse(time.RFC1123, pubDate)
-	// we use the parsed time only if we had no errors parsing it.
+	// We use the parsed time only if we had no errors parsing it.
 	if err == nil {
 		return pubDateTimeParsed
 	}
 
-	// try another format.
-	// torrentfreak rss feed format:
+	// Try another format.
+	//
+	// Torrentfreak RSS feed format:
+	//
 	// Sun, 30 Jun 2013 21:26:26 +0000
+	//
 	// Mon, 10 Jun 2013 21:04:57 +0000
 	pubDateTimeParsed, err = time.Parse(time.RFC1123Z, pubDate)
-	// we use the parsed time only if we had no errors parsing it.
+	// We use the parsed time only if we had no errors parsing it.
 	if err == nil {
 		return pubDateTimeParsed
 	}
 
-	// slashdot rdf format.
-	// 2015-03-03T21:29:00+00:00
+	// Slashdot RDF format: 2015-03-03T21:29:00+00:00
+	//
 	// NOTE: RFC3339 is not exactly this it seems?
 	pubDateTimeParsed, err = time.Parse(time.RFC3339, pubDate)
 	if err == nil {
@@ -241,13 +249,13 @@ func GetItemPubDate(pubDate string) time.Time {
 	if !config.Quiet {
 		log.Printf("No format worked for date [%s] - using default - NOW", pubDate)
 	}
+
 	return time.Now()
 }
 
-// ParseFeedXML takes the raw xml and returns a struct describing the
-// feed data.
+// ParseFeedXML takes the raw XML and returns a struct describing the feed.
 func ParseFeedXML(data []byte) (*Channel, error) {
-	// It is possible for us to not have valid XML. In such a case, the xml
+	// It is possible for us to not have valid XML. In such a case, the XML
 	// Decode function will not always complain. One way for this to happen is if
 	// you do not specify what tag the XML must start with.
 	err := looksLikeXML(data)
@@ -269,7 +277,7 @@ func ParseFeedXML(data []byte) (*Channel, error) {
 		errRDF)
 }
 
-// looksLikeXML applies some simple checks that we have an XML document.
+// looksLikeXML applies some simple checks to know if we have an XML document.
 func looksLikeXML(data []byte) error {
 	prefix := `<?xml version="1.0" encoding="`
 
@@ -290,18 +298,20 @@ func looksLikeXML(data []byte) error {
 func parseAsRSS(data []byte) (*Channel, error) {
 	// Decode from XML.
 
-	// To see how Unmarshal() works, refer to the documentation.
-	// Basically we have to tag the struct fields in the special format as in the
-	// package structs.
+	// To see how Unmarshal() works, refer to the documentation. Basically we
+	// have to tag the struct fields in the special format as in the package
+	// structs.
 	rssXML := RSSXML{}
 
 	// We can use xml.Unmarshal() except in cases where we need to convert between
 	// charsets. Which we want to be able to do, so we do not use Unmarshal().
+	//
 	// For example if we have:
 	// <?xml version="1.0" encoding="ISO-8859-1"?>
+	//
 	// Then we have to create an xml.Decoder and provide it a CharsetReader
-	// function.
-	// see http://stackoverflow.com/questions/6002619/unmarshal-an-iso-8859-1-xml-input-in-go
+	// function. See
+	// http://stackoverflow.com/questions/6002619/unmarshal-an-iso-8859-1-xml-input-in-go
 
 	// Decoder wants an io.Reader.
 	byteReader := bytes.NewBuffer(data)
@@ -333,7 +343,7 @@ func parseAsRSS(data []byte) (*Channel, error) {
 		log.Printf("Parsed channel as RSS [%s]", channel.Title)
 	}
 
-	// Should we report if there are no items at all as an error?
+	// TODO: Should we report if there are as an error?
 
 	for _, item := range rssXML.Channel.Items {
 		channel.Items = append(channel.Items,
@@ -351,7 +361,7 @@ func parseAsRSS(data []byte) (*Channel, error) {
 
 // parseAsRDF attempts to parse the buffer as if it contains an RDF feed.
 //
-// See parseAsRSS().
+// See parseAsRSS() for a similar function, but for RSS.
 func parseAsRDF(data []byte) (*Channel, error) {
 	rdfXML := RDFXML{}
 
@@ -397,40 +407,43 @@ func parseAsRDF(data []byte) (*Channel, error) {
 }
 
 // WriteFeedXML takes an RSSFeed and generates and writes an XML file.
-// this generates RSS 2.0.1.
-// see http://www.rssboard.org/rss-specification
-// validate the output files using:
+//
+// This function generates RSS 2.0.1.
+//
+// See http://www.rssboard.org/rss-specification
+//
+// You can validate the output files using:
 // http://www.rssboard.org/rss-validator
-// overall the XML structure is:
+//
+// Overall the XML structure is:
 // <rss><channel><item></item><item></item>...</channel></rss>
 //
-// NOTE: a note on timestamps. the rss spec says we should use
-//   RFC 822, but the time.RFC1123Z format looks closest to their
-//   examples, so I use that.
+// A note on timestamps: The RSS spec says we should use RFC 822, but the
+// time.RFC1123Z format looks closest to their examples, so I use that.
 func WriteFeedXML(feed *RSSFeed, filename string) error {
-	// top level element. version is required. we use 2.0 even though we
-	// are generating 2.0.1 as that, it seems, is the spec.
+	// Top level element. Version is required. We use 2.0 even though we are
+	// generating 2.0.1 as that, it seems, is the spec.
 	rss := RSSXML{
 		Version: "2.0",
 	}
 	rss.XMLName.Local = "rss"
 
-	// set up the channel metadata.
+	// Set up the channel metadata.
 	// <channel/>
 	//   <title/> Channel title
 	//   <link/> URL corresponding to channel
 	//   <description/> Phrase describing the channel
-	//   not required, but nice:
+	//   Not required, but nice to have:
 	//   <pubDate/> Publication date for the content
 	//   <lastBuildDate/> Last time content of channel changed
 	rss.Channel.Title = feed.Name
 	rss.Channel.Link = feed.URI
 	rss.Channel.Description = feed.Description
-	// XXX: technically these dates maybe should be different...
+	// TODO: Technically these dates maybe should be different...
 	rss.Channel.PubDate = feed.LastUpdateTime.Format(time.RFC1123Z)
 	rss.Channel.LastBuildDate = rss.Channel.PubDate
 
-	// set up each of our items.
+	// Set up each of our items.
 	//   <item/>
 	//     <title/> Title of the item
 	//     <link/> URL of the item
@@ -443,22 +456,23 @@ func WriteFeedXML(feed *RSSFeed, filename string) error {
 			Link:        item.URI,
 			Description: item.Description,
 			PubDate:     item.PublicationDate.Format(time.RFC1123Z),
-			// use the URI as guid - it should be uniquely identifying
-			// the post after all. NOTE: the guid has no required format
-			// other than it is intended to be unique.
+			// Use the URI as GUID. It should be uniquely identifying the post after
+			// all. Note the GUID has no required format other than it is intended to
+			// be unique.
 			GUID: item.URI,
 		}
+
 		rss.Channel.Items = append(rss.Channel.Items, itemXML)
 	}
 
-	// build the xml data.
+	// Build the XML.
 	xmlBody, err := xml.MarshalIndent(rss, "", "  ")
 	if err != nil {
 		log.Printf("Failed to marshal xml: %s", err)
 		return err
 	}
 
-	// add the xml header <?xml .. ?>
+	// Add the XML header <?xml .. ?>
 	xmlHeader := []byte(xml.Header)
 	var xmlDoc []byte
 	for _, v := range xmlHeader {
@@ -468,7 +482,6 @@ func WriteFeedXML(feed *RSSFeed, filename string) error {
 		xmlDoc = append(xmlDoc, v)
 	}
 
-	// write our file.
 	err = ioutil.WriteFile(filename, xmlDoc, 0644)
 	if err != nil {
 		log.Printf("Failed to write file [%s]: %s", filename, err)
@@ -481,9 +494,8 @@ func WriteFeedXML(feed *RSSFeed, filename string) error {
 	return nil
 }
 
-// RetrieveFeeds finds rss feeds from the database.
+// RetrieveFeeds finds RSS feeds from the database.
 func RetrieveFeeds(db *sql.DB) ([]RSSFeed, error) {
-	// retrieve the feeds from the database.
 	query := `
 SELECT
 id, name, uri, update_frequency_seconds, last_update_time
@@ -497,8 +509,8 @@ ORDER BY name
 		return nil, err
 	}
 
-	// build our slice of feeds.
 	var feeds []RSSFeed
+
 	for rows.Next() {
 		var id int64
 		var name string
@@ -533,16 +545,15 @@ ORDER BY name
 	return feeds, nil
 }
 
-// GetDurationUntilNextUpdateForDisplay builds a string describing how
-// long until the next update.
-// for example, '2 hours'
+// GetDurationUntilNextUpdateForDisplay builds a string describing how long
+// until the next update. For example, '2 hours'
 func (feed RSSFeed) GetDurationUntilNextUpdateForDisplay() (string, error) {
-	// we want to know how long since our last update as we will compare
-	// this with how long our update frequency is.
+	// We want to know how long since our last update as we will compare this
+	// with how long our update frequency is.
 	timeSinceLastUpdate := time.Now().Sub(feed.LastUpdateTime)
 
-	// take our update frequency and subtract the time since our last update
-	// to find the duration until the next update.
+	// Take our update frequency and subtract the time since our last update to
+	// find the duration until the next update.
 	nextUpdateTimeSeconds := float64(feed.UpdateFrequencySeconds) -
 		timeSinceLastUpdate.Seconds()
 
@@ -550,32 +561,34 @@ func (feed RSSFeed) GetDurationUntilNextUpdateForDisplay() (string, error) {
 	hours := int(minutes / 60)
 	minutesInt := int(minutes)
 
-	// it is possible for us to be past the time where we should have
-	// updated (if update script has not run, or there is a problem).
-	// just say that we should be updating now.
+	// It is possible for us to be past the time where we should have updated (if
+	// update script has not run, or there is a problem). Just say that we should
+	// be updating now/next time.
 	if nextUpdateTimeSeconds < 0 {
 		return "the next update", nil
 	}
+
 	if hours > 0 {
 		if hours == 1 {
 			return "1 hour", nil
 		}
 		return fmt.Sprintf("%d hours", hours), nil
 	}
+
 	if minutesInt == 1 {
 		return "1 minute", nil
 	}
+
 	return fmt.Sprintf("%d minutes", minutesInt), nil
 }
 
-// GetDurationSinceUpdateForDisplay builds a string describing how long ago
-// the last update was.
-// for example, '2 hours'.
+// GetDurationSinceUpdateForDisplay builds a string describing how long ago the
+// last update was. For example, '2 hours'.
 func (feed RSSFeed) GetDurationSinceUpdateForDisplay() string {
-	// now - last update time.
+	// Now minus last update time.
 	difference := time.Now().Sub(feed.LastUpdateTime)
 
-	// hours and minutes are given as floating - we want to round them.
+	// Hours and minutes are given as floating. Round them.
 	hours := int(difference.Hours())
 	minutes := int(difference.Minutes())
 	if hours > 0 {
@@ -584,21 +597,24 @@ func (feed RSSFeed) GetDurationSinceUpdateForDisplay() string {
 		}
 		return fmt.Sprintf("%d hours", hours)
 	}
+
 	if minutes == 1 {
 		return "1 minute"
 	}
+
 	return fmt.Sprintf("%d minutes", minutes)
 }
 
-// GetUpdateFrequencyForDisplay builds a string suitable for the user
-// out of our update frequency seconds.
+// GetUpdateFrequencyForDisplay builds a string suitable for the user out of our
+// update frequency seconds.
 func (feed RSSFeed) GetUpdateFrequencyForDisplay() string {
 	updateMinutes := feed.UpdateFrequencySeconds / 60
 	updateHours := updateMinutes / 60
 
-	// we give a rough idea of the time rather than being too specific.
-	// for example if we have over an hour then we just say there are
-	// x hour(s) and say nothing about the minute(s).
+	// We give a rough idea of the time rather than being too specific.
+	//
+	// For example if we have over an hour then we just say there are x hour(s)
+	// and say nothing about the minute(s).
 	var updateString string
 	if updateHours > 0 {
 		if updateHours == 1 {
@@ -613,5 +629,6 @@ func (feed RSSFeed) GetUpdateFrequencyForDisplay() string {
 			updateString = fmt.Sprintf("%d minutes", updateMinutes)
 		}
 	}
+
 	return updateString
 }
